40a184fc6cbf3d299e4893369c88db0ed0441159
xuwb <xuwb@superb-pay.com>
2024-12-02 15:42:42 +0800

    局部广告

--- src/apps/com.mipay.wallet.ts
+++ src/apps/com.mipay.wallet.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   groups: [
     {
       key: 0,
-      name: '钱包-广告',
+      name: '局部广告',
       activityIds:
         'com.xiaomi.jr.app.MiFinanceActivity',
       rules: '@ImageView < View < View < View < View < FrameLayout < [vid="flutter_container"]',
