3db95183a773c789400e7eb29e62a651f11b0e5f
xuwb <xuwb@superb-pay.com>
2024-12-02 09:42:34 +0800

    定位提示-定位请求

--- src/apps/com.htinns.ts
+++ src/apps/com.htinns.ts
@@ -7,15 +7,11 @@ export default defineAppConfig({
   groups: [
     {
       key: 1,
-      name: '定位提示',
+      name: '定位提示-定位请求',
       desc: '关闭扫码投屏提醒弹窗',
-      activityIds: [
-        'com.htinns.reactnative.v2.RNContainerActivity',
-      ],
-      snapshotUrls: 'https://i.gkd.li/import/17971393',// 
-      rules: [
-        '@Button < View <2 View < View < View < View < View < View <2 View < WebView < WebView < FrameLayout < ViewGroup <3 ViewGroup < ViewGroup < FrameLayout < LinearLayout < [id="android:id/content"]', // 1686970236642
-      ],
+      activityIds: 'com.htinns.reactnative.v2.RNContainerActivity',
+      snapshotUrls: 'https://i.gkd.li/import/17971393',
+      rules: '@Button < View <2 View < View < View < View < View < View <2 View < WebView < WebView < FrameLayout < ViewGroup <3 ViewGroup < ViewGroup < FrameLayout < LinearLayout < [id="android:id/content"]',
     },
   ],
 });
