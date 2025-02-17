61c5f3789465605865f3bb2eedf4cc42b0340809
xuwb <xuwb@superb-pay.com>
2025-02-17 15:33:08 +0800

    193

--- src/apps/com.htinns.ts
+++ src/apps/com.htinns.ts
@@ -9,10 +9,12 @@ export default defineAppConfig({
       key: 1,
       name: '定位提示-定位请求',
       desc: '关闭扫码投屏提醒弹窗',
-      activityIds: 'com.htinns.reactnative.v2.RNContainerActivity',
-      snapshotUrls: 'https://i.gkd.li/import/17971393',
-      rules:
-        '@Button < View <2 View < View < View < View < View < View <2 View < WebView < WebView < FrameLayout < ViewGroup <3 ViewGroup < ViewGroup < FrameLayout < LinearLayout < [id="android:id/content"]',
+      rules: [
+        {
+          matches: '@Button < View <2 View < View < View < View < View < View <2 View < WebView < WebView < FrameLayout < ViewGroup <3 ViewGroup < ViewGroup < FrameLayout < LinearLayout < [id="android:id/content"]',
+          snapshotUrls: ['https://i.gkd.li/import/17971393'],
+        },
+      ],,
     },
   ],
 });
---
+++ src/apps/com.jiaohua_browser.ts
@@ -0,0 +1,22 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.jiaohua_browser',
+  name: 'JMComic2',
+  groups: [
+    {
+      key: 1,
+      name: '进入',
+      activityIds: 'com.jiaohua_browser.MainActivity',
+      rules: '[text="我保證我已滿18歲！確定進入！"]',
+      snapshotUrls: 'https://i.gkd.li/import/18821510',
+    },
+    {
+      key: 2,
+      name: '关闭广告',
+      activityIds: 'com.jiaohua_browser.MainActivity',
+      rules: '[text=""]',
+      snapshotUrls: 'https://i.gkd.li/import/18821525',
+    }
+  ],
+});
