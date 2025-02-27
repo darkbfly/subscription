d7e5098b4397702100d5fcc078466a6cb8eb71c3
xuwb <xuwb@superb-pay.com>
2025-02-27 09:27:23 +0800

    1

---
+++ src/apps/cn.kuwo.player.ts
@@ -0,0 +1,26 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'cn.kuwo.player	',
+  name: '酷我音乐',
+  groups: [
+    {
+      key: 1,
+      name: '分段广告',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@TextView <3 View < View <4 View <3 View <3 WebView < WebView < [vid="pull_to_fresh"]',
+      snapshotUrls: 'https://i.gkd.li/import/19000088',
+    },
+    {
+      key: 2,
+      name: '分段广告',
+      quickFind: true,
+      actionMaximum: 1,
+      resetMatch: 'app',
+      rules: '@TextView <3 View <4 View <3 View <3 WebView < WebView < [vid="pull_to_fresh"]',
+      snapshotUrls: 'https://i.gkd.li/import/19000104',
+    },
+  ],
+});
