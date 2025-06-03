5f881ecf12a011d0aed53fb0914d0b59739a9b2f
xuwb <xuwb@superb-pay.com>
2025-06-03 14:01:52 +0800

    删除重复或不使用的

--- src/apps/com.kuaishou.nebula.ts
+++ src/apps/com.kuaishou.nebula.ts
@@ -3,15 +3,20 @@ import { defineAppConfig } from '../types';
 export default defineAppConfig({
   id: 'com.kuaishou.nebula',
   name: '快手极速版',
-  groups: [
-    {
-      key: 1,
-      name: '卡片式广告',
-      actionMaximum: 1,
-      resetMatch: 'app',
-      activityIds: 'com.yxcorp.gifshow.HomeActivity',
-      rules: '[id="com.kuaishou.nebula:id/positive"][text="我知道了"]',
-      snapshotUrls: 'https://i.gkd.li/import/13196316',
-    },
-  ],
+  deprecatedKeys: [0],
+  groups: [ {
+    key: 1,
+    name: '卡片式广告',
+    activityIds: [
+      'com.yxcorp.gifshow.HomeActivity',
+    ],
+    rules: '@Image < View < View <2 View <7 View < WebView < WebView < [vid="webView"]',
+  }, {
+    key: 2,
+    name: '卡片式广告',
+    activityIds: [
+      'com.yxcorp.gifshow.HomeActivity',
+    ],
+    rules: '@Image < View < View <2 View <6 View < WebView < WebView < [vid="webView"]',
+  }],
 });
